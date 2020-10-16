27b7a0a2642a844fc8fa2059e3fcc3a9b7baf0d3
Andrew Murray <radarhere@users.noreply.github.com>
2020-10-16 22:01:22 +1100

    apt

--- .ci/build.sh
+++ .ci/build.sh
@@ -2,10 +2,7 @@
 
 export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew install python@3.8
-python3.8 -m pip install img2pdf==0.2.3
-python3.8 -m pip install Pillow==7.2.0
+sudo apt install img2pdf
 img2pdf -o sample.pdf Tests/images/hopper.jpg
-ls -la
 ls -la /usr/bin/img2pdf
 cat /usr/bin/img2pdf
