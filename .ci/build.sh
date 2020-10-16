bae4e9ba7062dc89cfd5fcac1eb5ea7579700c16
Andrew Murray <radarhere@users.noreply.github.com>
2020-10-16 21:11:19 +1100

    install

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew install python3.8
+python3.8 -m pip install img2pdf
+img2pdf -o sample.pdf Tests/images/hopper.jpg
