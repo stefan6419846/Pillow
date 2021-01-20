38f4d4874286ccba6a543690ae81f125ab485620
Frankie Dintino <fdintino@gmail.com>
2021-01-20 12:26:49 -0500

    avif: Use C99-style designated initializers

--- src/_avif.c
+++ src/_avif.c
@@ -671,36 +671,14 @@ static struct PyMethodDef _encoder_methods[] = {
 
 // AvifDecoder type definition
 static PyTypeObject AvifEncoder_Type = {
-    PyVarObject_HEAD_INIT(NULL, 0) "AvifEncoder", /*tp_name */
-    sizeof(AvifEncoderObject),                    /*tp_size */
-    0,                                            /*tp_itemsize */
-    /* methods */
-    (destructor)_encoder_dealloc, /*tp_dealloc*/
-    0,                            /*tp_print*/
-    0,                            /*tp_getattr*/
-    0,                            /*tp_setattr*/
-    0,                            /*tp_compare*/
-    0,                            /*tp_repr*/
-    0,                            /*tp_as_number */
-    0,                            /*tp_as_sequence */
-    0,                            /*tp_as_mapping */
-    0,                            /*tp_hash*/
-    0,                            /*tp_call*/
-    0,                            /*tp_str*/
-    0,                            /*tp_getattro*/
-    0,                            /*tp_setattro*/
-    0,                            /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT,           /*tp_flags*/
-    0,                            /*tp_doc*/
-    0,                            /*tp_traverse*/
-    0,                            /*tp_clear*/
-    0,                            /*tp_richcompare*/
-    0,                            /*tp_weaklistoffset*/
-    0,                            /*tp_iter*/
-    0,                            /*tp_iternext*/
-    _encoder_methods,             /*tp_methods*/
-    0,                            /*tp_members*/
-    0,                            /*tp_getset*/
+    // clang-format off
+    PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "AvifEncoder",
+    // clang-format on
+    .tp_basicsize = sizeof(AvifEncoderObject),
+    .tp_dealloc = (destructor)_encoder_dealloc,
+    .tp_flags = Py_TPFLAGS_DEFAULT,
+    .tp_methods = _encoder_methods,
 };
 
 // AvifDecoder methods
@@ -712,36 +690,15 @@ static struct PyMethodDef _decoder_methods[] = {
 
 // AvifDecoder type definition
 static PyTypeObject AvifDecoder_Type = {
-    PyVarObject_HEAD_INIT(NULL, 0) "AvifDecoder", /*tp_name */
-    sizeof(AvifDecoderObject),                    /*tp_size */
-    0,                                            /*tp_itemsize */
-    /* methods */
-    (destructor)_decoder_dealloc, /*tp_dealloc*/
-    0,                            /*tp_print*/
-    0,                            /*tp_getattr*/
-    0,                            /*tp_setattr*/
-    0,                            /*tp_compare*/
-    0,                            /*tp_repr*/
-    0,                            /*tp_as_number */
-    0,                            /*tp_as_sequence */
-    0,                            /*tp_as_mapping */
-    0,                            /*tp_hash*/
-    0,                            /*tp_call*/
-    0,                            /*tp_str*/
-    0,                            /*tp_getattro*/
-    0,                            /*tp_setattro*/
-    0,                            /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT,           /*tp_flags*/
-    0,                            /*tp_doc*/
-    0,                            /*tp_traverse*/
-    0,                            /*tp_clear*/
-    0,                            /*tp_richcompare*/
-    0,                            /*tp_weaklistoffset*/
-    0,                            /*tp_iter*/
-    0,                            /*tp_iternext*/
-    _decoder_methods,             /*tp_methods*/
-    0,                            /*tp_members*/
-    0,                            /*tp_getset*/
+    // clang-format off
+    PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "AvifDecoder",
+    // clang-format on
+    .tp_basicsize = sizeof(AvifDecoderObject),
+    .tp_itemsize = 0,
+    .tp_dealloc = (destructor)_decoder_dealloc,
+    .tp_flags = Py_TPFLAGS_DEFAULT,
+    .tp_methods = _decoder_methods,
 };
 
 PyObject *
@@ -828,10 +785,9 @@ PyInit__avif(void) {
 
     static PyModuleDef module_def = {
         PyModuleDef_HEAD_INIT,
-        "_avif",     /* m_name */
-        NULL,        /* m_doc */
-        -1,          /* m_size */
-        avifMethods, /* m_methods */
+        .m_name = "_avif",
+        .m_size = -1,
+        .m_methods = avifMethods,
     };
 
     m = PyModule_Create(&module_def);
