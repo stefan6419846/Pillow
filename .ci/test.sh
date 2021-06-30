ea3a5e1cb0a52c43e6c1086541d2e685f96cf783
Andrew Murray <radarhere@users.noreply.github.com>
2021-06-30 21:58:04 +1000

    Test

--- .ci/test.sh
+++ .ci/test.sh
@@ -2,6 +2,4 @@
 
 set -e
 
-python3 -c "from PIL import Image"
-
-python3 -bb -m pytest -v -x -W always --cov PIL --cov Tests --cov-report term Tests $REVERSE
+python3 -c "from PIL import Image;from PIL import Image;im = Image.new('RGB', (100, 100));im.convert('01234567890123456789')"
