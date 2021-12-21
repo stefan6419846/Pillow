84d5c36594939530be62c1647d0b66be88d9149f
Andrew Murray <radarhere@users.noreply.github.com>
2021-12-22 09:38:54 +1100

    run

--- Tests/test_file_jpeg2k.py
+++ Tests/test_file_jpeg2k.py
@@ -18,14 +18,19 @@ EXTRA_DIR = "Tests/images/jpeg2000"
 
 pytestmark = skip_unless_feature("jpg_2000")
 
-test_card = Image.open("Tests/images/test-card.png")
-test_card.load()
-
 # OpenJPEG 2.0.0 outputs this debugging message sometimes; we should
 # ignore it---it doesn't represent a test failure.
 # 'Not enough memory to handle tile data'
 
 
 def test_16bit_monochrome_jp2_like_tiff():
-    with Image.open("Tests/images/16bit.cropped.tif") as tiff_16bit:
-        assert_image_similar_tofile(tiff_16bit, "Tests/images/16bit.cropped.jp2", 1e-3)
+    with Image.open("Tests/images/16bit.cropped.jp2") as x:
+        px = x.load()
+        print("first", px[0, 0])
+        print("x mode", x.mode)
+        print("---")
+        with Image.open("Tests/images/16bit.cropped.tif") as tiff_16bit:
+            px = tiff_16bit.load()
+            print("second", px[0, 0])
+            x.save("Tests/images/16bit.cropped.png")
+            assert_image_similar(tiff_16bit, x, 1e-3)
